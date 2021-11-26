53bd08fa80cc57c584e2fba832c7d89551d1af89
MansiGodhani <81357299+MansiGodhani@users.noreply.github.com>
2021-11-26 12:09:26 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,114 @@
+const question = [
+    {
+        qno: 1,
+        que: "What does the REPL stand for?",
+        a: "Read Eval Print Loop.",
+        b: "Research Eval Program Learn.",
+        c: "Read Earn Point Learn.",
+        d: "Read Eval Point Loop.",
+        ans : "a",
+        desc : "a is the correct option. REPL stands for Read Eval Print Loop."
+
+    },
+    {
+        qno: 2,
+        que: "Which of the following command is used to start a REPL session?",
+        a: "$ node",
+        b: "$ node start",
+        c: "$ node repl",
+        d: "$ node console",
+        ans : "a",
+        desc : "a is the correct option, We can start REPL simply by running node on shell/console without any argument."
+
+
+    },
+    {
+        qno: 3,
+        que: "In which language is Node.js written?",
+        a: "JavaScript",
+        b: "C",
+        c: "C++",
+        d: "All of the above",
+        ans : "d",
+        desc :"d is Correct option, Node.js is written in JavaScript, C, C++, and CoffeeScript language."
+
+    },
+    {
+        qno: 4,
+        que: "Which of the following command is used to install the Node.js express module?",
+        a: "$ npm install express",
+        b: "$ node install express",
+        c: "$ install express",
+        d: "None of the above.",
+        ans: "a",
+        desc: "a is Correct option, The $ npm install express command is used to install the Node.js express module."
+
+    },
+    {
+        qno: 5,
+        que: "Which of the following extension is used to save the Node.js files?",
+        a: ".js",
+        b: ".node",
+        c: ".java",
+        d: ".txt",
+        ans: "a",
+        desc: "a is Correct option, TThe '.js' extension is used to save the Node.js files."
+
+    },
+    {
+        qno: 6,
+        que: "Swift is which kind of language?",
+        a: "expose",
+        b: "module",
+        c: "exports",
+        d: "All of the above.",
+        ans: "c",
+        desc: "c is Correct option. The Node.js modules can be exposed using module.exports."
+
+    },
+    
+    {
+        qno: 7,
+        que: "Which of the following module is not a built-in node module?",
+        a: "zlib",
+        b: "https",
+        c: "dgram",
+        d: "fsread",
+        ans: "d",
+        desc: "d is Correct option, The 'fsread' is not a built-in node module."
+
+    },
+    {
+        qno: 8,
+        que: "What is the default scope in the Node.js application?",
+        a: "Global",
+        b: "Local",
+        c: "Global Function",
+        d: "Local to object",
+        ans: "b",
+        desc: "b is Correct option, Local is the default scope in the Node.js application."
+
+    },
+    {
+        qno: 9,
+        que: " Which of the following shortcut command is used to kill a process in Node.js?",
+        a: "Ctrl + B",
+        b: "Ctrl + K",
+        c: "Ctrl + T",
+        d: "Ctrl + C",
+        ans: "d",
+        desc: "d is Correct option, The Ctrl + C command is used to kill a process in Node.js."
+
+    },
+    {
+        qno: 10,
+        que: "Which of the following areas Node.js is not advised to be used?",
+        a: "Single Page Applications",
+        b: "MobJSON APIs based Applicationsiface",
+        c: "CPU intensive applications",
+        d: "Data-Intensive Real-time Applications (DIRT)",
+        ans: "c",
+        desc: "c is Correct option, It is not advisable to use Node.js for CPU-intensive applications."
+    }
+]
+module.exports = question;
